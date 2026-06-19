6da2c5efb1bdc7b7032019e7b8cb6a6eb4932a64
chirazseni <vucxdftc@gmail.com>
2026-06-19 21:58:06 +0100

    new edit

---
+++ product.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  description: String,
+  image: String,
+  colorImages: [String],
+  colors: [String],
+  sizes: [String],
+  category: String,
+  isBestSeller: { type: Boolean, default: false }
+});
+
+module.exports = mongoose.model('Product', productSchema);
